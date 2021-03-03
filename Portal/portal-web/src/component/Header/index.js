a0d68c0aa5d5a371823208267242e4d90709a65c
alvescvinicius <alvescvinicius@gmail.com>
2021-03-03 00:53:27 -0300

    Components creation and add Bootstrap

--- Portal/portal-web/src/component/Header/index.js
+++ Portal/portal-web/src/component/Header/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
-function Header(){
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from 'react-bootstrap';
+
+function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header id="main-header">
+            <div className="header-content justify-content-center">
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Sobre</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Projetos</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Contato</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
         </header>
     );
 }
 
-export default Header; 
+export default Header;
