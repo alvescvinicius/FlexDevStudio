c7f2f7fd2181047c70ee2ede67775c259267ed56
alvescvinicius <alvescvinicius@gmail.com>
2021-03-13 00:55:34 -0300

    Implementacao inicial de consulta a API de BITCOIN

--- Portal/ctp-monitor-web/src/components/Home/index.js
+++ Portal/ctp-monitor-web/src/components/Home/index.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bitcoin: {}
+        };
+
+    }
+
+    componentDidMount() {
+
+        /**
+         * -----------------------------------------------------------------------
+         * - 13/03/2021 - BITCOIN - cointradermonitor.com
+         * -----------------------------------------------------------------------
+         * - API encontrada para monitorar a variacao do BITCOIN de forma GRATUITA.
+         */
+        let url = 'https://cointradermonitor.com/api/pbb/v1/ticker';
+        fetch(url)
+            .then((r) => r.json())
+            .then((json) => {
+                let state = this.state;
+                state.bitcoin = json;
+                this.setState(state);
+                console.log(json);
+                console.log(this.state.bitcoin);
+            })
+    }
+
     render() {
         return (
-            <div>
-                <h3>Home</h3>
-            </div >
+            <div className="container">
+                <header>
+                    <strong>React bitcoin</strong>
+                    <p>{this.state.bitcoin.pair}</p>
+                </header>
+
+
+            </div>
         );
     }
 }
