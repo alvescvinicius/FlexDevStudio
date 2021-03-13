8596f26d23090fb281328d2f4778c030c7102106
alvescvinicius <alvescvinicius@gmail.com>
2021-03-12 23:23:08 -0300

    Instalacao e configuracao de router e componentes

--- Portal/ctp-monitor-web/src/App.js
+++ Portal/ctp-monitor-web/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>Teste de execução do projeto CTP-MONITOR-WEB</div>
-  );
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+
+/*
+  Leave on final imports to not override
+*/
+import './global.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ Portal/ctp-monitor-web/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+
+        <header>
+
+            <h3>Header</h3>
+
+        </header>
+
+    );
+}
+
+export default Header;
